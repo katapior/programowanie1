d4d8b2a70a371b07a2195853bcf864c475aa8084
katapior <katapior@gmail.com>
2018-09-03 21:40:26 +0200

    silnia

---
+++ zadanie/zadanie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie
+{
+    class Program
+    {
+        public static long Silnia (long a)
+        {
+            if (a > 1)
+            {
+                return  a * Silnia(a - 1);
+            }
+            else return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            long silnia = Silnia(6);
+            Console.WriteLine($"Silnia = {silnia}");
+        }
+    }
+}
